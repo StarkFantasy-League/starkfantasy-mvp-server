df07b62dfbe1c28aac2b21d4c0d25d771381e305
Ezeh20 <ranter1570@gmail.com>
2025-04-27 22:29:28 +0100

    chore: added two additional endpoints

--- src/sports/modules/player/controllers/player.controller.ts
+++ src/sports/modules/player/controllers/player.controller.ts
@@ -1,30 +1,39 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { CricketPlayerService } from '../service/player.service';
-import { CricketPlayer } from "src/schema";
-
+import { CricketPlayer } from 'src/schema';
+import { PlayerStats, PlayerRadarStats } from 'src/types/player.types';
 
 @Controller('cricket-player')
-export class CricketPlayerController{
-    constructor (private readonly playerService: CricketPlayerService){}
+export class CricketPlayerController {
+  constructor(private readonly playerService: CricketPlayerService) {}
 
-@Post()
-create (@Body() dto: CricketPlayer){
+  @Post()
+  create(@Body() dto: CricketPlayer): Promise<CricketPlayer> {
     return this.playerService.create(dto);
-}
+  }
 
-@Get()
-findAll(){
+  @Get()
+  findAll(): Promise<CricketPlayer[]> {
     return this.playerService.findAll();
-}
+  }
 
-@Get (':id')
-findOne(@Param ('id') id:string){
+  @Get('stats')
+  getPlayerStats(): Promise<PlayerStats[]> {
+    return this.playerService.getPlayerStats();
+  }
+
+  @Get('radar/:id')
+  getPlayerRadarStats(@Param('id') id: string): Promise<PlayerRadarStats> {
+    return this.playerService.getPlayerRadarStats(id);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<CricketPlayer> {
     return this.playerService.findOne(id);
-}
+  }
 
-@Delete (':id')
-delete (@Param('id') id:string){
-return this.playerService.delete(id);
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<void> {
+    return this.playerService.delete(id);
+  }
 }
-}
-
