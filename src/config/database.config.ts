01d1c71270c5ced4e222721d3cf878c40d716574
BravarDev <brayanvafer@gmail.com>
2025-05-01 14:08:01 -0600

    configuration for deployment

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,16 +3,16 @@ import { ConfigService } from '@nestjs/config';
 import { CricketMatch, CricketPlayer, CricketPool, CricketTeam, PlayerPerformance, BetsOptions, SpecialBet, CricketPlayerHistorial } from 'src/schema';
 
 export const getDatabaseConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
-  type: 'mssql',
+  type: 'mariadb',
   host: configService.get('DB_HOST') || 'localhost',
   port: parseInt(configService.get('DB_PORT') || "1433"),
   username: configService.get('DB_USERNAME') || 'sa',
   password: configService.get('DB_PASSWORD') || 'admin123!',
   database: configService.get('DB_DATABASE') || 'StarkFantasy',
-  options: {
-    encrypt: false,
-    trustServerCertificate: true,
-  },
+  // options: {
+  //   encrypt: false,
+  //   trustServerCertificate: true,
+  // },
   entities: [CricketMatch, CricketTeam, CricketPool, PlayerPerformance, CricketPlayer, BetsOptions, SpecialBet, CricketPlayerHistorial],
   synchronize: false,
 });
